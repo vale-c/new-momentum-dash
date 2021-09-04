c5fb42bc1a4ce21d558a0f272dbd8ae49707d798
unknown <valent95@gmail.com>
2021-09-04 11:45:46 +0200

    removed useMemo

--- src/components/Weather/index.jsx
+++ src/components/Weather/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import {Description, Temperature, Town, WeatherBox, WeatherIcon } from './styled'
 
